36e870897162bd216056405ffa5bd8d8dcaf7588
song <songshiji@bupt.edu.cn>
2019-01-20 15:46:37 +0800

    spring-boot学习 初始化： 1、修改maven  setting.xml配置文件，修改仓库位置配置、添加阿里云maven库，在 pom.xml 中添加 Spring Boot 相关的父级依赖，在 dependencies 添加构建 Web 项目相关的依赖 2、创建控制器类Controller、创建引导类：springboot项目入口 3、运行引导类、访问localhost:8080/hello测试成功

---
+++ src/main/java/com/song/HelloMailAppliation.java
@@ -0,0 +1,15 @@
+package com.song;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//@SpringBootApplication 用于标识一个主程序类,说明当前是Spring Boot项目
+@SpringBootApplication
+public class HelloMailAppliation {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloMailAppliation.class,args);
+    }
+
+
+}
---
+++ src/main/java/com/song/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.song.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+@Controller
+public class HelloController {
+
+
+
+        @ResponseBody
+        @RequestMapping("/hello")
+        public String hello() {
+            return "helloword---";
+        }
+
+
+
+}
